ed89c77c538e516700a0f05df80f16689763a8b0
MistyField <20301050198@fudan.edu.cn>
2023-07-05 20:26:29 +0800

    feat: create project with vuepress

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: 'reco',
+  themeConfig: {
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      },
+      socialLinks: [     // 信息栏展示社交信息
+        { icon: 'reco-github', link: 'https://github.com/recoluan' },
+        { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
+      ]
+    }
+  }
+}
---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+---
+title: 【vue】跨域解决方案之proxyTable
+date: 2017-12-28
+categories:
+  - frontEnd
+tags:
+  - vue
+---
