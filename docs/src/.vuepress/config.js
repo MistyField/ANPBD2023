aea0d22b8ef116c1edc05df76d830e995d8e2a12
Zhiyue Chen <20301050198@fudan.edu.cn>
2023-07-06 02:03:55 +0800

    feat: modified navbar

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,20 +1,19 @@
 module.exports = {
+  title: 'ANPBD',
   theme: 'reco',
+  head:[
+    ['link', { rel: 'stylesheet', href: '/iconfont/iconfont.css' }],
+    ['link', { rel: 'icon', href: '/fudan_logo.svg' }],
+  ],
   themeConfig: {
-    // 博客配置
-    blogConfig: {
-      category: {
-        location: 2,     // 在导航栏菜单中所占的位置，默认2
-        text: 'Category' // 默认文案 “分类”
-      },
-      tag: {
-        location: 3,     // 在导航栏菜单中所占的位置，默认3
-        text: 'Tag'      // 默认文案 “标签”
-      },
-      socialLinks: [     // 信息栏展示社交信息
-        { icon: 'reco-github', link: 'https://github.com/recoluan' },
-        { icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan' }
-      ]
-    }
+    logo: '/fudan_logo.svg',
+    subSidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/', icon: 'iconfont icon-home' },
+      { text: 'Organizer', link: '/organizer/', icon: 'iconfont icon-user' },
+      { text: 'Speakers', link: '/speakers/', icon: 'iconfont icon-chalkboard-teacher'},
+      { text: 'Agenda', link: '/agenda/', icon: 'iconfont icon-calendar-alt' },
+      { text: 'FAQ', link: '/faq/', icon: 'iconfont icon-question-circle' },
+    ]
   }
 }
